373ce3aad53baf1d5ff2b84820c019d5ddee71f0
fdoargolo <felipeargolo75@gmail.com>
2024-08-27 12:02:51 -0300

    Entity User and resource

---
+++ src/main/java/com/felipeArgolo/project/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.felipeArgolo.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipeArgolo.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Felipe", "Felipe@gmail.com", "99999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
